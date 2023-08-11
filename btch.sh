4407e7d5eac0bb731091d5a4f98e6dbb48f0a9bf
Amatheus <137468880+Amatheus7@users.noreply.github.com>
2023-08-11 20:29:36 +0800

    Beach Test Bash Script
    Btch.

---
+++ btch.sh
@@ -0,0 +1,33 @@
+echo "Bitch test"
+sleep 1
+echo "What's your name?"
+
+read name
+
+echo "What's your age?"
+
+read age 
+
+sleep 1
+
+bitch=$((( $RANDOM % 15) + $age ))
+
+sleep 1
+echo "Calculating"
+sleep 2
+echo "...................."
+sleep 1
+echo "***................."
+sleep 1
+echo "******.............."
+sleep 1
+echo "**********.........."
+sleep 1
+echo "*****************..."
+sleep 1
+echo "********************"
+sleep 2
+echo "Calculating complete"
+sleep 1
+echo "$name, you will get a bitch at $bitch years old"
+
